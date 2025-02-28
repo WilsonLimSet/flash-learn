c6319f9e70b497f45199292d357b871c775e0412
WilsonLimSet <wilsonlimsetiawan@gmail.com>
2025-03-01 01:48:56 +0800

    v1 should be g but a few bugs later..,

--- next.config.js
+++ next.config.js
@@ -8,14 +8,8 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Remove assetPrefix if you're not using a custom domain
-  // assetPrefix: '',
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  // Add this to ensure static files are handled correctly
-  images: {
-    unoptimized: true,
   },
 };
 
